4250e9fef014c583192defbfa78bca4a5f8462b6
Ketan Vaidya <ketan.vaidya@gmail.com>
2018-08-12 23:47:55 +0530

    initial commit of full repository

---
+++ ReadMe.txt
@@ -0,0 +1,7 @@
+
+To Directly Run the project :
+-----------------------------
+cd D:\Ketan\WebDesign\Practice\
+> npm run start
+
+
---
+++ js/script.js
@@ -0,0 +1,6 @@
+
+// each span element can be accessed and modified.  
+
+let code = document.querySelector('.code-string');
+
+//Need some logic and code here.
